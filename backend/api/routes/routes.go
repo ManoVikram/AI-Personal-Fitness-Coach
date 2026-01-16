d28b974c52149f57203c58878f709a60a6364d75
Mano Vikram <manovik18@gmail.com>
2026-01-17 01:10:36 +0530

    Added the chat endpoint to the routes file

--- backend/api/routes/routes.go
+++ backend/api/routes/routes.go
@@ -1,8 +1,15 @@
 package routes
 
 import (
+	"github.com/ManoVikram/AI-Personal-Fitness-Coach/backend/api/handlers"
 	"github.com/ManoVikram/AI-Personal-Fitness-Coach/backend/api/services"
 	"github.com/gin-gonic/gin"
 )
 
-func RegisterRoutes(server *gin.Engine, services *services.Services) {}
+func RegisterRoutes(server *gin.Engine, services *services.Services) {
+	v1 := server.Group("/api/v1")
+	{
+		// POST request to chat with AI coach
+		v1.POST("/chat", handlers.CoachChatHandler(services))
+	}
+}
