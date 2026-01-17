6b8020351658e33a198c8622b150b336bc671a66
Mano Vikram <manovik18@gmail.com>
2026-01-17 13:38:16 +0530

    Added an endpoint to generate a workout plan

--- backend/api/routes/routes.go
+++ backend/api/routes/routes.go
@@ -11,5 +11,8 @@ func RegisterRoutes(server *gin.Engine, services *services.Services) {
 	{
 		// POST request to chat with AI coach
 		v1.POST("/chat", handlers.CoachChatHandler(services))
+
+		// POST request to generate workout plan
+		v1.POST("/workout/generate", handlers.WorkoutGeneratorHandler(services))
 	}
 }
