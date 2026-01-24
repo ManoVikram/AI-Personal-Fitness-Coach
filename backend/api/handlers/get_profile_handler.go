0ffbeb61b69bf9ef9d2f0dd40a83b58ddf1976a3
Mano Vikram <manovik18@gmail.com>
2026-01-24 15:54:46 +0530

    Built a handler to get user profile

---
+++ backend/api/handlers/get_profile_handler.go
@@ -0,0 +1,29 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/ManoVikram/AI-Personal-Fitness-Coach/backend/api/repository"
+	"github.com/ManoVikram/AI-Personal-Fitness-Coach/backend/api/services"
+	"github.com/gin-gonic/gin"
+)
+
+func GetProfileHandler(services *services.Services) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Step 1 - Get user ID from JWT (set by auth middleware)
+		userID, exists := c.Get("user_id")
+		if !exists {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "User not authenticated"})
+			return
+		}
+
+		// Step 2 - Get user profile from DB
+		userProfile, err := repository.GetUserProfile(c.Request.Context(), userID.(string))
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "User profile not found. Please create a profile."})
+			return
+		}
+
+		c.JSON(http.StatusOK, userProfile)
+	}
+}
