b537e8c76d248d9cc3d6768de36feb1e22a2c169
Mano Vikram <manovik18@gmail.com>
2026-01-24 15:59:45 +0530

    Updated the user profile model with JSON keys

--- backend/api/models/user_profile.go
+++ backend/api/models/user_profile.go
@@ -1,11 +1,11 @@
 package models
 
 type UserProfile struct {
-	UserID       string
-	Name         string
-	Age          int32
-	FitnessGoal  string
-	FitnessLevel string
-	Equipment    []string
-	Gender       string
+	UserID       string   `json:"userID"`
+	Name         string   `json:"name"`
+	Age          int32    `json:"age"`
+	FitnessGoal  string   `json:"fitnessGoal"`
+	FitnessLevel string   `json:"fitnessLevel"`
+	Equipment    []string `json:"equipment"`
+	Gender       string   `json:"gender"`
 }
